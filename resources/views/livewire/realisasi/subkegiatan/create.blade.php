4ec9a02d2cd484a948949534b1e63ef75237d03c
Chairul Anam <chairulanamart16@gmail.com>
2023-11-20 11:19:42 +0800

    V1'

--- resources/views/livewire/realisasi/subkegiatan/create.blade.php
+++ resources/views/livewire/realisasi/subkegiatan/create.blade.php
@@ -1,17 +1,20 @@
-<tr class="bg-info text-white">
+<tr class=" text-dark" style="background-color: rgb(206, 203, 203);">
     <td>Triwulan</td>
-    <td>Target & Satuan</td>
+    <td>Capaian & Satuan</td>
     <td>Pagu</td>
     <td>Rincian</td>
     <td>File</td>
     <td>
-        <i class="fas fa-cog fa-fw"></i>
+        <button class="btn btn-sm btn-transparent" data-toggle="collapse" href="#subkegiatan-{{ $subkegiatan->uuid }}"
+            role="button" aria-expanded="false" aria-controls="subkegiatan-{{ $subkegiatan->uuid }}">
+            <i class="fas fa-plus fa-fw"></i>
+        </button>
     </td>
 </tr>
-<tr style="background-color: rgb(218, 218, 218);">
+<tr style="background-color: rgb(218, 218, 218);" class="collapse" id="subkegiatan-{{ $subkegiatan->uuid }}">
     {{-- <td colspan="5"></td> --}}
     <td class="border ">
-        <div class="input-group">
+        <div class="input-group m-0">
             <select class="form-control form-control-sm" wire:model='triwulan'>
                 <option>.::PILIH TRIWULAN::.</option>
                 <option value="I">I</option>
@@ -20,49 +23,49 @@
                 <option value="IV">IV</option>
             </select>
             @error('triwulan')
-            <span class="text-danger">{{ $message }}</span>
+                <span class="text-danger">{{ $message }}</span>
             @enderror
             <input type="date" class="form-control" placeholder="Tanggal" wire:model='tanggal'
                 value="{{ date('Y-m-d') }}">
             @error('target')
-            <span class="text-danger">{{ $message }}</span>
+                <span class="text-danger">{{ $message }}</span>
             @enderror
         </div>
     </td>
     <td class="border">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="TARGET" wire:model='target'>
+        <div class="input-group m-0">
+            <input type="text" class="form-control" placeholder="Capaian" wire:model='target'>
             @error('target')
-            <span class="text-danger">{{ $message }}</span>
+                <span class="text-danger">{{ $message }}</span>
             @enderror
             /
-            <input type="text" class="form-control" placeholder="SATUAN" wire:model='satuan'>
+            <input type="text" class="form-control" placeholder="Satuan" wire:model='satuan'>
             @error('target')
-            <span class="text-danger">{{ $message }}</span>
+                <span class="text-danger">{{ $message }}</span>
             @enderror
         </div>
     </td>
     <td class="border">
-        <div class="input-group">
+        <div class="input-group m-0">
             <span class="btn">
                 Rp.
             </span>
             <input type="number" class="form-control" wire:model='pagu'>
         </div>
         @error('pagu')
-        <span class="text-danger">{{ $message }}</span>
+            <span class="text-danger">{{ $message }}</span>
         @enderror
     </td>
     <td class="border">
         <input type="text" class="form-control" placeholder="RINCIAN" wire:model='rincian'>
         @error('rincian')
-        <span class="text-danger">{{ $message }}</span>
+            <span class="text-danger">{{ $message }}</span>
         @enderror
     </td>
     <td>
         <input type="file" class="form-control" placeholder="FILE" wire:model='file'>
         @error('file')
-        <span class="text-danger">{{ $message }}</span>
+            <span class="text-danger">{{ $message }}</span>
         @enderror
     </td>
     <td class="border">
--- resources/views/livewire/realisasi/subkegiatan/table.blade.php
+++ resources/views/livewire/realisasi/subkegiatan/table.blade.php
@@ -16,80 +16,128 @@
 
                 {{-- data --}}
                 @forelse ($subkegiatans as $subkegiatan)
-                <tr class="text-primary">
-                    <td>
-                        <i class="fas fa-arrow-right"></i>
-                        {{ $subkegiatan->kode }}
-                    </td>
-                    <td>
-                        {{ $subkegiatan->title }}
-                    </td>
-                    <td>
-                        {{ $subkegiatan->pegawai->nama }}
-                    </td>
-                    <td>
-                        @currency($subkegiatan->pagu_awal)
-                    </td>
-                    <td colspan="2">
-                        @php
-                        $pagu_terserap = 0;
-                        foreach ($subkegiatan->realisasi_subkegiatan as $rs) {
-                        $pagu_terserap += $rs->pagu;
-                        }
-                        @endphp
-                        <h6 class="m-0">
-                            <strong
-                                class="{{ $subkegiatan->pagu_awal == $pagu_terserap ? 'text-success' : 'text-danger' }}">
-                                @currency($pagu_terserap)
-                            </strong>
-                        </h6>
-                    </td>
-                </tr>
-                @include('livewire.realisasi.subkegiatan.create')
-                @foreach ($subkegiatan->realisasi_subkegiatan()->orderBy('triwulan', 'ASC')->get() as $rs)
-                <tr style="background-color: rgb(218, 218, 218);">
-                    <td>
-                        <select style="width: 100% !important;" class="form-control">
-                            <option value="I" {{ $rs->triwulan == 'I' ? 'selected' : '' }}>I</option>
-                            <option value="II" {{ $rs->triwulan == 'II' ? 'selected' : '' }}>II</option>
-                            <option value="III" {{ $rs->triwulan == 'III' ? 'selected' : '' }}>III</option>
-                            <option value="IV" {{ $rs->triwulan == 'IV' ? 'selected' : '' }}>IV</option>
-                        </select>
-                    </td>
-                    <td class="d-flex justify-content-center">
-                        <input type="text" value="{{ $rs->target }}" class="form-control"
-                            wire:blur="update('{{ $rs->uuid }}', 'target', $event.target.value)">
-                        /
-                        <input type="text" value="{{ $rs->satuan }}" class="form-control"
-                            wire:blur="update('{{ $rs->uuid }}', 'satuan', $event.target.value)">
-                    </td>
-                    <td>
-                        <div class="d-flex">
-                            <input type="text" value="{{ $rs->pagu }}" class="form-control"
-                                wire:blur="update('{{ $rs->uuid }}', 'pagu', $event.target.value)"> =
-                            <strong>@currency($rs->pagu)</strong>
-                        </div>
-                    </td>
-                    <td>
-                        <input type="text" value="{{ $rs->keterangan }}" class="form-control"
-                            wire:blur="update('{{ $rs->uuid }}', 'keterangan', $event.target.value)">
-                    </td>
-                    <td>
-                        <a href="{{ url('storage') . '/' . $rs->file }}">
-                            <i class="ik ik-file"></i>
-                        </a>
-                    </td>
-                    <td>
-                        <button class="btn btn-sm btn-light" wire:click='destroy("{{ $rs->uuid }}")'>
-                            <i class="ik ik-trash text-danger"></i>
-                        </button>
-                    </td>
-                </tr>
-                @endforeach
+                    <tr class=" text-white" style="background-color: rgb(255, 191, 112);">
+                        <td>
+                            <i class="fas fa-arrow-right"></i>
+                            {{ $subkegiatan->kode }}
+                        </td>
+                        <td>
+                            {{ $subkegiatan->title }}
+                        </td>
+                        <td>
+                            {{ $subkegiatan->pegawai->nama }}
+                        </td>
+                        <td>
+                            @currency($subkegiatan->pagu_awal)
+                        </td>
+                        <td colspan="2">
+                            @php
+                                $pagu_terserap = 0;
+                                foreach ($subkegiatan->realisasi_subkegiatan as $rs) {
+                                    $pagu_terserap += $rs->pagu;
+                                }
+                            @endphp
+                            <h6 class="m-0">
+                                <strong
+                                    class="{{ $subkegiatan->pagu_awal == $pagu_terserap ? 'text-success' : 'text-danger' }}">
+                                    @currency($pagu_terserap)
+                                </strong>
+                            </h6>
+                        </td>
+                    </tr>
+                    @include('livewire.realisasi.subkegiatan.create')
+                    @foreach ($subkegiatan->realisasi_subkegiatan()->orderBy('triwulan', 'ASC')->get() as $rs)
+                        <tr style="background-color: rgb(218, 218, 218);">
+                            <td class="">
+                                <div class="d-flex">
+                                    <select style="width: 100% !important;" class="form-control">
+                                        <option value="I" {{ $rs->triwulan == 'I' ? 'selected' : '' }}>I</option>
+                                        <option value="II" {{ $rs->triwulan == 'II' ? 'selected' : '' }}>II</option>
+                                        <option value="III" {{ $rs->triwulan == 'III' ? 'selected' : '' }}>III
+                                        </option>
+                                        <option value="IV" {{ $rs->triwulan == 'IV' ? 'selected' : '' }}>IV</option>
+                                    </select>
+                                </div>
+                            </td>
+                            <td class="d-flex justify-content-center">
+                                <input type="text" value="{{ $rs->target }}" class="form-control"
+                                    wire:blur="update('{{ $rs->uuid }}', 'target', $event.target.value)">
+                                /
+                                <input type="text" value="{{ $rs->satuan }}" class="form-control"
+                                    wire:blur="update('{{ $rs->uuid }}', 'satuan', $event.target.value)">
+                            </td>
+                            <td>
+                                <div class="d-flex">
+                                    <input type="text" value="{{ $rs->pagu }}" class="form-control"
+                                        wire:blur="update('{{ $rs->uuid }}', 'pagu', $event.target.value)"> =
+                                    <strong>@currency($rs->pagu)</strong>
+                                </div>
+                            </td>
+                            <td>
+                                <input type="text" value="{{ $rs->keterangan }}" class="form-control"
+                                    wire:blur="update('{{ $rs->uuid }}', 'keterangan', $event.target.value)">
+                            </td>
+                            <td class="text-center">
+                                <a href="{{ url('storage') . '/' . $rs->file }}">
+                                    <i class="ik ik-file"></i>
+                                </a>
+                            </td>
+                            <td>
+                                <div class="btn-group">
+                                    <button class="btn btn-sm btn-transparent" data-toggle="collapse"
+                                        href="#collapse-{{ $rs->uuid }}" role="button" aria-expanded="false"
+                                        aria-controls="collapse-{{ $rs->uuid }}">
+                                        <i class="fas fa-plus fa-fw"></i>
+                                    </button>
+                                    <button class="btn btn-sm btn-transparent"
+                                        wire:click='destroy("{{ $rs->uuid }}")'>
+                                        <i class="ik ik-trash text-danger"></i>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr style="background-color: rgb(237, 237, 237);" class="collapse"
+                            id="collapse-{{ $rs->uuid }}">
+                            <td class="text-right">
+                                <i class="ik ik-corner-down-right fa-2x"></i>
+                            </td>
+                            <td colspan="">
+                                <input type="text" class="form-control" placeholder="Deskripsi Rincian...">
+                            </td>
+                            <td>
+                                <div class="input-group m-0">
+                                    <span class="btn">
+                                        Rp.
+                                    </span>
+                                    <input type="number" class="form-control" placeholder="Pagu">
+                                </div>
+                                @error('pagu')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </td>
+                            <td>
+                                <input type="date" class="form-control" placeholder="Tanggal" wire:model='tanggal'
+                                    value="{{ date('Y-m-d') }}">
+                            </td>
+                            <td>
+                                <input type="file" class="form-control" placeholder="FILE">
+                                @error('file')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-transaparent"
+                                    wire:click='destroy("{{ $rs->uuid }}")'>
+                                    <i class="ik ik-trash text-danger"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
                 @empty
-                <tr class="">
-                    <td class="text-center" colspan="6">Sub Kegiatan Masih Kosong, Mohon Tambahkan dimenu DPA</td>
-                </tr>
+                    <tr class="">
+                        <td class="text-center" colspan="6">Sub Kegiatan Masih Kosong, Mohon Tambahkan dimenu DPA
+                        </td>
+                    </tr>
                 @endforelse
 
             </tbody>
