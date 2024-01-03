96f2488c813d5ed38aef392e454e030addb4acf2
Zephyr-E <lauwxelakhmed@gmail.com>
2024-01-04 07:52:15 +0800

    total anggaran laporan

--- resources/views/panel/pages/laporan/partials/program.blade.php
+++ resources/views/panel/pages/laporan/partials/program.blade.php
@@ -1,3 +1,12 @@
+@php
+$pagu_target = 0;
+$pagu_realisasi = 0;
+$pagu_twi = 0;
+$pagu_twii = 0;
+$pagu_twiii = 0;
+$pagu_twiv = 0;
+@endphp
+
 {{-- baris program --}}
 @forelse ($programs as $prog)
 @php
@@ -8,8 +17,10 @@ $rows = $prog->indikator_program->count() != 0 ? $prog->indikator_program->count
 $rp = $prog->realisasi_program()->orderBy('triwulan', 'ASC')->get();
 
 // menghitung total capaian & pagu tw i-iv
-$persentase_kinerja = !is_null($rp->last()) ? number_format(($rp->last()->capaian / $prog->target * 100), 1, ',', '') : 0;
-$persentase_pagu = $prog->sumTotalSubKeg() != 0 ? number_format($prog->sumTotal() / $prog->sumTotalSubKeg() * 100, 1, ',', '') : 0;
+$persentase_kinerja = !is_null($rp->last()) ? number_format(($rp->last()->capaian / $prog->target * 100), 1, ',', '') :
+0;
+$persentase_pagu = $prog->sumTotalSubKeg() != 0 ? number_format($prog->sumTotal() / $prog->sumTotalSubKeg() * 100, 1,
+',', '') : 0;
 
 // untuk menghitung total pagu masing-masing triwulan
 $pagu_triwulan = 0;
@@ -17,6 +28,9 @@ $pagu_triwulan = 0;
 // untuk looping realisasi triwulan agar dinamis
 $triwulan = ['I', 'II', 'III', 'IV'];
 
+// menambahkan total pagu program
+$pagu_target += $prog->sumTotalSubKeg();
+$pagu_realisasi += $prog->sumTotal();
 @endphp
 <tr class="program">
     <td rowspan="{{ $rows }}">
@@ -54,9 +68,28 @@ $triwulan = ['I', 'II', 'III', 'IV'];
         $pagu_triwulan / $prog->sumTotalSubKeg()
         * 100, 1, ',', '') : 0 }} %
     </td>
+
+    {{-- total triwulan --}}
+    @php
+    switch ($tw) {
+    case 'I':
+    $pagu_twi += $prog->sumTotalRincian($tw);
+    break;
+    case 'II':
+    $pagu_twii += $prog->sumTotalRincian($tw);
+    break;
+    case 'III':
+    $pagu_twiii += $prog->sumTotalRincian($tw);
+    break;
+    case 'IV':
+    $pagu_twiv += $prog->sumTotalRincian($tw);
+    break;
+    }
+    @endphp
+
     @endforeach
 
-        {{-- total triwulan --}}
+    {{-- total triwulan --}}
     {{-- total kinerja --}}
     <td class="text-center nowrap" rowspan="{{ $rows }}">
         {{ (!is_null($rp->last()) ? $rp->last()->capaian : 0) . ' ' . $prog->satuan }}
@@ -95,3 +128,30 @@ $triwulan = ['I', 'II', 'III', 'IV'];
     <td class="text-center" colspan="25">DATA KOSONG</td>
 </tr>
 @endforelse
+
+<tr class="header">
+    <td class="text-center" colspan="3">Total Anggaran : </td>
+    <td class="text-center">@currency(($pagu_target != 0) ? $pagu_target : 0)</td>
+    {{-- triwulan I --}}
+    <td class="text-center" colspan="2">Total Triwulan I : </td>
+    <td class="text-center">@currency(($pagu_twi != 0) ? $pagu_twi : 0)</td>
+    <td>{{ ($pagu_twi != 0) ? number_format($pagu_twi / $pagu_target * 100, 1, ',', '.') : 0 }} %</td>
+    {{-- triwulan II --}}
+    <td class="text-center" colspan="2">Total Triwulan II : </td>
+    <td class="text-center">@currency(($pagu_twii != 0) ? ($pagu_twii + $pagu_twi) : 0)</td>
+    <td>{{ ($pagu_twii != 0) ? number_format(($pagu_twii + $pagu_twi) / $pagu_target * 100, 1, ',', '.') : 0 }} %</td>
+    {{-- triwulan III --}}
+    <td class="text-center" colspan="2">Total Triwulan III : </td>
+    <td class="text-center">@currency(($pagu_twiii != 0) ? ($pagu_twiii + $pagu_twii + $pagu_twi) : 0)</td>
+    <td>{{ ($pagu_twiii != 0) ? number_format(($pagu_twiii + $pagu_twii + $pagu_twi) / $pagu_target * 100, 1, ',', '.') : 0 }} %</td>
+    {{-- triwulan IV --}}
+    <td class="text-center" colspan="2">Total Triwulan IV : </td>
+    <td class="text-center">@currency(($pagu_twiv != 0) ? ($pagu_twiv + $pagu_twiii + $pagu_twii + $pagu_twi) : 0)</td>
+    <td>{{ ($pagu_twiv != 0) ? number_format(($pagu_twiv + $pagu_twiii + $pagu_twii + $pagu_twi) / $pagu_target * 100, 1, ',', '.') : 0 }} %</td>
+
+    <td class="text-center" colspan="2">Total Realisasi : </td>
+    <td class="text-center">@currency($pagu_realisasi)</td>
+    <td>{{ ($pagu_realisasi != 0) ? number_format($pagu_realisasi / $pagu_target * 100, 1, ',', '.') : 0 }} %</td>
+
+    <td></td>
+</tr>
