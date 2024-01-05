a01278cef92d3f5c27c065b2666c593c3a1f6012
Zephyr-E <lauwxelakhmed@gmail.com>
2024-01-05 08:28:32 +0800

    akses menu

--- resources/views/panel/templates/inc/sidebar.blade.php
+++ resources/views/panel/templates/inc/sidebar.blade.php
@@ -6,25 +6,27 @@
                 <a href="{{ route('dashboard') }}"><i class="ik ik-bar-chart-2"></i><span>Dashboard</span></a>
             </div>
             @if (auth()->user()->rule != 'non-admin')
-            <div class="nav-item {{ Route::currentRouteName() == 'laporan' ? 'active' : '' }}">
-                <a href="{{ route('laporan') }}"><i class="ik ik-printer"></i><span>Monev</span></a>
-            </div>
-            <div class="nav-lavel">Perencanaan</div>
-            <div
-                class="nav-item {{ in_array(Route::currentRouteName(), ['dpa.index', 'dpa.kegiatan', 'dpa.subkegiatan']) ? 'active' : '' }}">
-                <a href="{{ route('dpa.index') }}"><i class="ik ik-book"></i><span>DPA</span></a>
-            </div>
-            <div
-                class="nav-item {{ in_array(Route::currentRouteName(), ['realisasi.index', 'realisasi.kegiatan', 'realisasi.subkegiatan', 'realisasi.rincian-belanja']) ? 'active' : '' }}">
-                <a href="{{ route('realisasi.index') }}"><i class="ik ik-book-open"></i><span>Realisasi</span></a>
-            </div>
-            @endif
-            @if (auth()->user()->rule == 'admin')
-            <div class="nav-lavel">Master Data</div>
-            <div
-                class="nav-item {{ in_array(Route::currentRouteName(), ['pegawai.index', 'pegawai.create', 'pegawai.edit']) ? 'active' : '' }}">
-                <a href="{{ route('pegawai.index') }}"><i class="ik ik-users"></i><span>Pegawai</span></a>
-            </div>
+                <div class="nav-item {{ Route::currentRouteName() == 'laporan' ? 'active' : '' }}">
+                    <a href="{{ route('laporan') }}"><i class="ik ik-printer"></i><span>Monev</span></a>
+                </div>
+                <div class="nav-lavel">Perencanaan</div>
+                    @if (auth()->user()->rule != 'kabid')
+                        <div
+                            class="nav-item {{ in_array(Route::currentRouteName(), ['dpa.index', 'dpa.kegiatan', 'dpa.subkegiatan']) ? 'active' : '' }}">
+                            <a href="{{ route('dpa.index') }}"><i class="ik ik-book"></i><span>DPA</span></a>
+                        </div>
+                    @endif
+                <div
+                    class="nav-item {{ in_array(Route::currentRouteName(), ['realisasi.index', 'realisasi.kegiatan', 'realisasi.subkegiatan', 'realisasi.rincian-belanja']) ? 'active' : '' }}">
+                    <a href="{{ route('realisasi.index') }}"><i class="ik ik-book-open"></i><span>Realisasi</span></a>
+                </div>
+                @if (auth()->user()->rule == 'admin')
+                    <div class="nav-lavel">Master Data</div>
+                    <div
+                        class="nav-item {{ in_array(Route::currentRouteName(), ['pegawai.index', 'pegawai.create', 'pegawai.edit']) ? 'active' : '' }}">
+                        <a href="{{ route('pegawai.index') }}"><i class="ik ik-users"></i><span>Pegawai</span></a>
+                    </div>
+                @endif
             @endif
         </nav>
     </div>
